062c6c15456388730190c0cc37b8ada96b07b212
HovhannesTutyan <atutyanaz@gmail.com>
2021-10-03 16:18:21 +0400

    color swap commit

--- test.js
+++ test.js
@@ -1,1 +1,14 @@
+const btn = document.getElementById("btn")
+const color = document.querySelector('.color')
+const colors = ['red', 'green', 'yellow']
+ 
+btn.addEventListener('click', function(){
+    const randomNumber = getRandomNumber()
+    document.body.style.backgroundColor = colors[randomNumber]
+    color.textContent = colors[randomNumber]
 
+});
+
+function getRandomNumber(){
+    return Math.floor(Math.random() * colors.length);
+}
